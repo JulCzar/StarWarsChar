0c9c2261d564858d05d36f2d4300f21476c8f5f6
JulCzar <julczar21@outlook.com>
2019-11-02 19:36:38 -0300

    mudança de referências

--- script/variaveis.js
+++ script/variaveis.js
@@ -2,9 +2,9 @@ const myInit = { method: 'GET', mode: 'cors', cache: 'default' };
 const proxPage = document.getElementById("proximo")
 const antPage = document.getElementById("anterior")
 const body = document.querySelector("#container")
-let man = `<img class="genero" src="img/man.svg" alt="sexo masculino">`
-let woman = `<img class="genero" src="img/woman.svg" alt="sexo feminino">`
-let na = `<img class="genero" src="img/na.svg" alt="sexo indefinido">`
+const man = `<img class="genero" src="img/man.svg" alt="sexo masculino">`
+const woman = `<img class="genero" src="img/woman.svg" alt="sexo feminino">`
+const na = `<img class="genero" src="img/na.svg" alt="sexo indefinido">`
 
 let cores = {
     "black": "preto",
