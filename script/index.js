c66db90e1769d021cb7b39a97e7e7a8e881fc087
JulCzar <julczar21@outlook.com>
2019-11-02 19:02:46 -0300

    diversos ajustes na dinâmica da página

--- script/index.js
+++ script/index.js
@@ -1,33 +1,50 @@
-(function() {
+(function () {
     api()
 })()
-
-
 
 function api(atual = 'https://swapi.co/api/people/?format=json') {
     if (typeof atual != `string`) atual = atual.getAttribute(`page`)
     fetch(atual, myInit)
         .then(response => response.json())
-        .then(function(response) {
+        .then(async function (response) {
             const { next, previous } = response
             proxPage.setAttribute("page", next || undefined)
             antPage.setAttribute("page", previous || undefined)
             proxPage.hidden = !next
             antPage.hidden = !previous
             body.innerHTML = ""
-            response.results.forEach(element => {
-                body.insertAdjacentHTML('beforeend', makeRow(element))
+            response.results.forEach(async element => {
+                body.insertAdjacentHTML('beforeend', await makeRow(element))
             });
         });
 }
 
 async function planetaNat(item) {
     item += "?format=json"
-    const response = await fetch(item, myInit).json()
-    return response.name
+    let response = await fetch(item, myInit)
+    let data = await response.json()
+    return data.name
 }
 
 function calc(item, divisor) {
     item /= divisor
-    return item + "m"
+    return item
+}
+
+async function makeRow(item) {
+    const planet = await planetaNat(item.homeworld)
+    let altura = (item.height == "unknown")?"desconhecida":(calc(item.height, 100) + "m")
+    let peso = (item.mass == "unknown")?"desconhecido":(item.mass + "kg")
+    let nascimento = (item.birth_year == "unknown")?"desconhecido":item.birth_year
+    return `
+    <div class="personagem">
+        <div class="nome">${(item.name) + '<span class="sexo">' + ((item.gender != "n/a") ? ((item.gender == "male") ? man : woman) : na) + '</span>' + '<span class="homeland">' + (planet) + "</span>"}</div>
+        <div class="altura dados">${altura}</div>
+        <div class="peso dados">${peso}</div>
+        <div class="CabeloCor dados">${item.hair_color}</div>
+        <div class="peleCor dados">${item.skin_color}</div>
+        <div class="olhoCor dados">${item.eye_color}</div>
+        <div class="nasceu dados">${nascimento}</div>
+    </div>
+    `
 }
--- script/variaveis.js
+++ script/variaveis.js
@@ -1,25 +1,17 @@
-let man = `<img class="genero" src="img/man.svg" alt="sexo masculino">`
-let woman = `<img class="genero" src="img/woman.svg" alt="sexo feminino">`
-let na = `<img class="genero" src="img/na.svg" alt="sexo indefinido">`
 const myInit = { method: 'GET', mode: 'cors', cache: 'default' };
 const proxPage = document.getElementById("proximo")
 const antPage = document.getElementById("anterior")
 const body = document.querySelector("#container")
+let man = `<img class="genero" src="img/man.svg" alt="sexo masculino">`
+let woman = `<img class="genero" src="img/woman.svg" alt="sexo feminino">`
+let na = `<img class="genero" src="img/na.svg" alt="sexo indefinido">`
 
-
-
-function makeRow(item) {
-    return `
-    <div class="personagem">
-        <div class="nome">${item.name}</div>
-        <div class="altura">${calc(item.height, 100)}</div>
-        <div class="peso">${item.mass}</div>
-        <div class="CabeloCor">${item.hair_color}</div>
-        <div class="peleCor">${item.skin_color}</div>
-        <div class="olhoCor">${item.eye_color}</div>
-        <div class="nasceu">${item.bith_year}</div>
-        <div class="sexo">${(item.gender != "n/a")?((item.gender == "male")?man:woman):na}</div>
-        <div class="plantNatal">${planetaNat(item.homeworld)}</div>
-    </div>
-    `
+let cores = {
+    "black": "preto",
+    "white": "branco",
+    "yellow": "amarelo",
+    "golden": "dourado",
+    "green": "verde",
+    "blonde": "loiro",
+    "blue": "azul"
 }
